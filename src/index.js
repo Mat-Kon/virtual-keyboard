c418607c8d7919bceabd2eeae5496bece5479cf9
Mat-Kon <mat_kon@mail.ru>
2023-04-27 19:59:40 +0300

    feat: add rows in keyboard contain

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import {createHeader} from "./modules/createElements/header.js";
 import {createTextarea} from "./modules/createElements/textarea.js";
-import {createKeyboardContain} from "./modules/createElements/keyboardContain.js"
+import {createKeyboardContain, createKeyboardRows} from "./modules/createElements/keyboardContain.js"
 
 window.onload = () => {
-  createHeader();
-  createTextarea();
-  createKeyboardContain();
 };
+createHeader();
+createTextarea();
+createKeyboardContain();
+for (let i = 0; i < 5; i++) {
+  createKeyboardRows();
+}
+
--- src/modules/createElements/keyboardContain.js
+++ src/modules/createElements/keyboardContain.js
@@ -1,6 +1,18 @@
-export const createKeyboardContain = () => {
+const createKeyboardContain = () => {
   const textarea = document.querySelector(".text-window")
-  const keyboard = document.createElement("div");
-  keyboard.className = "keyboard wrapper";
+  const keyboard = createElement("div", "keyboard wrapper");
   textarea.after(keyboard);
 }
+
+const createKeyboardRows = () => {
+  const keyboard = document.querySelector(".keyboard");
+  const rows = createElement("div", "keyboard_rows");
+  keyboard.append(rows);
+}
+
+const createElement = (tag, className) => {
+  const element = document.createElement(`${tag}`);
+  element.className = `${className}`;
+  return element;
+}
+export {createKeyboardContain, createKeyboardRows};
